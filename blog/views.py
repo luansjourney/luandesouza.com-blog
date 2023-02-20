ac9d89cd2491f9e920cbb24c32ba5851ddd2d0ca
= <=>
2023-02-20 05:39:18 -0300

    ADD: new templates, styles and base static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "blog/index.html")
+
+def posts(request):
+    return render(request, "blog/all-posts.html")
+
+def post_detail(request):
+    pass

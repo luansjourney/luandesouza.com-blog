789f5c64d9fa20fd4ba4cec45fd14b73d20eb157
= <=>
2023-02-20 06:17:59 -0300

    ADD: detail post, dummy data

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index-page"),
     path('posts', views.posts, name="posts-page"),
-    path('posts/<slug>', views.post_detail, name="post-detail-page")
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,64 @@
+from datetime import date
+
 from django.shortcuts import render
+
+posts = [
+    {
+        "slug": "hike-mountain",
+        "image": "prof.png",
+        "author": "Luan",
+        "date": date(2023, 2, 20),
+        "title": "Mountain",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam consequuntur neque quos eius sint?",
+        "content": """Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam consequuntur neque quos eius sint?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam consequuntur neque quos eius sint?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam consequuntur neque quos eius sint?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam consequuntur neque quos eius sint? """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
 
 # Create your views here.
 
@@ -8,5 +68,8 @@ def index(request):
 def posts(request):
     return render(request, "blog/all-posts.html")
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+
+
+
+    return render(request, "blog/post-detail.html")

b259269b3a862b35a9d859096fea66baa38e743c
= <=>
2023-02-22 09:15:07 -0300

    Add: dummy data to test the views func

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         "slug": "hike-mountain",
         "image": "prof.png",
@@ -17,8 +17,8 @@ posts = [
     },
     {
         "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
+        "image": "prof.png",
+        "author": "Luan",
         "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
@@ -38,8 +38,8 @@ posts = [
     },
     {
         "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
+        "image": "prof.png",
+        "author": "Luan",
         "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
@@ -59,11 +59,19 @@ posts = [
     }
 ]
 
+#helpers
+
+def get_date(post):
+    return post['date']
+
 
 # Create your views here.
-
 def index(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
     return render(request, "blog/all-posts.html")

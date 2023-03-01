bb9988da221b4e7dbd491302af2dd36c1a4453ca
= <dsl.ing@outlook.com>
2023-02-28 22:08:47 -0300

    MODIFY: wsgi var name

--- luan_site/settings.py
+++ luan_site/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = getenv('SECRETKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '.vercel.app'
-]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
